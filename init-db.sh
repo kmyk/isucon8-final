941c5b6680ded495ba20aba208fc16dba82f4a6f
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-10-20 12:23:45 +0900

    init-db.sh をdockerはずし対応

--- init-db.sh
+++ init-db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-ISU_DB_USER=root
-ISU_DB_PASSWORD=root
+ISU_DB_USER=isucon
+ISU_DB_PASSWORD=isucon
 ISU_DB_HOST=127.0.0.1
-ISU_DB_PORT=13306
+ISU_DB_PORT=3306
 ISU_DB_NAME=isucoin
 
 mysql -u$ISU_DB_USER -p$ISU_DB_PASSWORD --host $ISU_DB_HOST --port $ISU_DB_PORT <<EOF
