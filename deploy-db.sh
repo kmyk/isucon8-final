f51148195d692003df2b8b074f77d67986c001ab
hikalium <hikalium@hikalium.com>
2018-10-20 13:43:51 +0900

    Add deploy scripts for db and its schema

---
+++ deploy-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+for ip in $@ ; do
+    for f in etc/my.cnf ; do
+        scp $f isucon@$ip:~/
+    done
+
+ssh isucon@${ip} <<EOF
+set -e
+sudo mv my.cnf /etc/
+sudo systemctl restart mysqld
+EOF
+
+done
