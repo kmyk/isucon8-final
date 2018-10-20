b73e357ea560f8785873b0f79d30b1891538e362
Keishi Kawada <megumish@exploitouri.st>
2018-10-20 12:43:14 +0900

    application deploy script

---
+++ env.sh
@@ -0,0 +1,7 @@
+ISU_APP_PORT=5000
+ISU_DB_HOST=172.16.51.4
+ISU_DB_PORT=3306
+ISU_DB_USER=isucon
+ISU_DB_PASSWORD=isucon
+ISU_DB_NAME=isucoin
+ISU_PUBLIC_DIR=/home/isucon/isucon2018-final/webapp/public/
