efb1ffa03859f9336921e4f50612245f8917d9ee
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-10-20 11:54:46 +0900

    DBのschema定義を単離

---
+++ init-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+ISU_DB_USER=root
+ISU_DB_PASSWORD=root
+ISU_DB_HOST=127.0.0.1
+ISU_DB_PORT=13306
+ISU_DB_NAME=isucoin
+
+mysql -u$ISU_DB_USER -p$ISU_DB_PASSWORD --host $ISU_DB_HOST --port $ISU_DB_PORT <<EOF
+DROP DATABASE ${ISU_DB_NAME};
+CREATE DATABASE ${ISU_DB_NAME};
+EOF
+mysql -u$ISU_DB_USER -p$ISU_DB_PASSWORD --host $ISU_DB_HOST --port $ISU_DB_PORT $ISU_DB_NAME < isucoin.sql
+mysql -u$ISU_DB_USER -p$ISU_DB_PASSWORD --host $ISU_DB_HOST --port $ISU_DB_PORT $ISU_DB_NAME < isucoin.dump.sql
